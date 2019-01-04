67dc1170bff8349f7703f1b0bd441c9afd7115ab
Tarlan Ismayilsoy <terlanismayilsoy@Terlans-MacBook.local>
2019-01-04 22:14:56 +0400

    xCode first commit

---
+++ window-shopper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by Tarlan Ismayilsoy on 1/4/19.
+//  Copyright © 2019 Tarlan Ismayilsoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
